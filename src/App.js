97a773dbdbfbeee5a3d4842f750e8dfc13d369df
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2020-03-04 22:29:42 +0100

    Create Dashboard component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Navbar from "./components/layouts/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Navbar />
+        <Switch>
+          <Route to="/" exact component={Dashboard} />
+        </Switch>
       </BrowserRouter>
     );
   }
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by MM</p>
+          <p className="grey-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt eum
+            consectetur, dolorem maxime sed, nulla autem commodi dolore, in ut
+            quae nesciunt. Magnam eius consectetur doloremque dolores, deleniti
+            sed reprehenderit.
+          </p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by MM</p>
+          <p className="grey-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt eum
+            consectetur, dolorem maxime sed, nulla autem commodi dolore, in ut
+            quae nesciunt. Magnam eius consectetur doloremque dolores, deleniti
+            sed reprehenderit.
+          </p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by MM</p>
+          <p className="grey-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt eum
+            consectetur, dolorem maxime sed, nulla autem commodi dolore, in ut
+            quae nesciunt. Magnam eius consectetur doloremque dolores, deleniti
+            sed reprehenderit.
+          </p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by MM</p>
+          <p className="grey-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt eum
+            consectetur, dolorem maxime sed, nulla autem commodi dolore, in ut
+            quae nesciunt. Magnam eius consectetur doloremque dolores, deleniti
+            sed reprehenderit.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
