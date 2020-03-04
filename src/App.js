3a9cfc6a4a177d2c7c37abaf8b36ccb93c461353
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2020-03-04 21:43:58 +0100

    Setting up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <BrowserRouter>
+        <div>Hello World!</div>
+      </BrowserRouter>
+    );
   }
 }
 
