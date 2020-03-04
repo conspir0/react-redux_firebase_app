384664602cd2a85f43df4f0c2b5deb3183075717
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2020-03-04 22:35:33 +0100

    Create ProjectSummary component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,59 +1,13 @@
 import React from "react";
 
+import ProjectSummary from "./ProjectSummary";
+
 const ProjectList = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by MM</p>
-          <p className="grey-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt eum
-            consectetur, dolorem maxime sed, nulla autem commodi dolore, in ut
-            quae nesciunt. Magnam eius consectetur doloremque dolores, deleniti
-            sed reprehenderit.
-          </p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by MM</p>
-          <p className="grey-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt eum
-            consectetur, dolorem maxime sed, nulla autem commodi dolore, in ut
-            quae nesciunt. Magnam eius consectetur doloremque dolores, deleniti
-            sed reprehenderit.
-          </p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by MM</p>
-          <p className="grey-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt eum
-            consectetur, dolorem maxime sed, nulla autem commodi dolore, in ut
-            quae nesciunt. Magnam eius consectetur doloremque dolores, deleniti
-            sed reprehenderit.
-          </p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by MM</p>
-          <p className="grey-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt eum
-            consectetur, dolorem maxime sed, nulla autem commodi dolore, in ut
-            quae nesciunt. Magnam eius consectetur doloremque dolores, deleniti
-            sed reprehenderit.
-          </p>
-        </div>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   );
 };
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ProjectSummary = () => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>Posted by MM</p>
+        <p className="grey-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt eum
+          consectetur, dolorem maxime sed, nulla autem commodi dolore, in ut
+          quae nesciunt. Magnam eius consectetur doloremque dolores, deleniti
+          sed reprehenderit.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSummary;
